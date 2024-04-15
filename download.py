9d1b635819c01e3418aad436de00ba2c94ee09b9
Trikzy7 <theoplebani2002@icloud.com>
2024-04-15 10:01:47 +0200

    [FEATURE] script to download asf images in .zip from a polygon

---
+++ download.py
@@ -0,0 +1,57 @@
+from multiprocessing import freeze_support
+
+import asf_search as asf
+from datetime import datetime
+from os import listdir
+from pathlib import Path
+import getpass
+from tqdm import tqdm
+import sys
+import argparse
+
+if __name__ == '__main__':
+    # Define the parser
+    parser = argparse.ArgumentParser(description='Download image data in .zip format from ASF')
+
+    # Arguments 
+    parser.add_argument('--outputPath', help='Absolute Path to directory where the downloaded files will be saved', required=True)
+    parser.add_argument('--username', help='Username to authentification in ASF', required=True)
+    parser.add_argument('--password', help='Password to authentification in ASF', required=True)
+    parser.add_argument('--polygon', help='Polygon you want images from', required=True)
+    parser.add_argument('--dateStart', help='Begining of the period you want download with format YYYY-MM-DD', required=True)
+    parser.add_argument('--dateEnd', help='End of the period you want download with format YYYY-MM-DD', required=True)
+
+
+    # Analyser Arguments 
+    args = parser.parse_args()
+
+    # Get the parameters
+    output_path = args.outputPath
+    username = args.username
+    password = args.password
+    polygon = args.polygon
+    date_start = args.dateStart
+    date_end = args.dateEnd
+
+
+    datetime_format = '%Y-%m-%d'
+
+
+    freeze_support()
+    
+    # username = input('Username:')
+    # password = getpass.getpass('Password:')
+    
+    user_pass_session = asf.ASFSession().auth_with_creds(username=username, password=password)
+    
+    results_with_zips = asf.search(
+        intersectsWith=polygon,
+        platform=asf.PLATFORM.SENTINEL1,
+        processingLevel=asf.PRODUCT_TYPE.GRD_HD,
+        start=datetime.strptime(date_start, datetime_format),
+        end=datetime.strptime(date_end, datetime_format))
+    
+    # results_with_zips[0:1].download(path='./downloads7', session=user_pass_session)
+    
+    for result_zip in tqdm(results_with_zips):
+        result_zip.download(path=output_path, session=user_pass_session)
