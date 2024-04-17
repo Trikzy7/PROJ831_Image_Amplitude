7288db5e0c786cd8fe319609adc88b2b785e23cc
Carlyne Barrachin <carlyne.barrachin@gmail.com>
2024-04-17 15:33:13 +0200

    [FIX] color and back-to-top arrow

--- frontend/src/app/configuration-map/configuration-map.component.ts
+++ frontend/src/app/configuration-map/configuration-map.component.ts
@@ -13,5 +13,17 @@ import {MapComponent} from "../map/map.component";
   styleUrl: './configuration-map.component.scss'
 })
 export class ConfigurationMapComponent {
-
+    ngOnInit() {
+        window.addEventListener('scroll', () => {
+            let backToTopButton = document.getElementById('backToTop');
+            if (window.pageYOffset > 100) { // Show backToTopButton after 100px
+                backToTopButton!.style.display = "block";
+            } else { // Hide backToTopButton
+                backToTopButton!.style.display = "none";
+            }
+        });
+    }
+    scrollToTop() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }
 }
